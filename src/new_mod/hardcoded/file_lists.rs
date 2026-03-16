95033c0afb52b4b1e3412299fe321d4c756d732f
marcel <marcel.hajek7@gmail.com>
2026-03-16 01:03:40 +0100

    ...

--- src/new_mod/hardcoded/file_lists.rs
+++ src/new_mod/hardcoded/file_lists.rs
@@ -11,7 +11,7 @@ project({name})
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD 20)
 
 add_library(${{PROJECT_NAME}}
     Private/{name}.cpp
