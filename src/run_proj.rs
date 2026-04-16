8e23154c6ce609f7a05cce1a7a807e57d8219242
marcel <marcel.hajek7@gmail.com>
2026-04-16 15:30:49 +0200

    ...

--- src/run_proj.rs
+++ src/run_proj.rs
@@ -1,19 +1,42 @@
-use std::process::Command;
+use std::process::{Command, exit};
 
-fn run_python(python_exec_name: &str) {
-    Command::new(python_exec_name)
-        .arg("run.py")
-        .spawn()
-        .unwrap_or_else(|e| panic!("running project failed with error {e}"));
-}
+use crate::utils::get_parent_directory;
 
 pub fn run_proj() {
-    let exec_name = match std::env::consts::FAMILY
-    {
-        "windows" => "python",
-        "unix" => "python3",
-        _ => panic!("unsupported OS family"),
-    };
+    Command::new("cmake")
+        .arg(".")
+        .arg("-B")
+        .arg("build")
+        .arg("-GNinja")
+        .spawn()
+        .unwrap_or_else(|e| {
+            eprintln!(
+                "Error when pre building: {e:?}"
+            );
 
-    run_python(exec_name);
+            exit(0);
+        });
+
+    Command::new("cmake")
+        .arg("--build")
+        .arg("build")
+        .spawn()
+        .unwrap_or_else(|e| {
+            eprintln!(
+                "Error when building: {e:?}"
+            );
+
+            exit(0);
+        });
+
+    Command::new(format!(
+        "./build/Source/Main/{}",
+        get_parent_directory()
+    ))
+    .spawn()
+    .unwrap_or_else(|e| {
+        eprintln!("Error when executing: {e:?}");
+
+        exit(0);
+    });
 }
