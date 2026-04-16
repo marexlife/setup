10a99af67ae10b85d7921835062343a89730c747
marcel <marcel.hajek7@gmail.com>
2026-04-16 18:10:26 +0200

    ...

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 cargo build --release
 rm -rf Project
-./target/release/setup new Project
+./target/release/setup new project
 cd Project
-./../target/release/setup mod Mod
+./../target/release/setup mod mod
 cd ..
 
 echo "export PATH=\"$PATH:$(pwd)/target/release\"" >> ~/.bashrc
