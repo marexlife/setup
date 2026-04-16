30fccae0319e1b399dbf2e63093e681588382328
marcel <marcel.hajek7@gmail.com>
2026-04-16 17:43:48 +0200

    ...

--- src/run_proj.rs
+++ src/run_proj.rs
@@ -1,21 +1,14 @@
-use std::process::{Command, Output, exit};
+use std::process::{Command, exit};
 
 use crate::utils::get_parent_directory;
 
-fn print_output(output: Output) {
-    println!(
-        "{}",
-        String::from_utf8(output.stdout).unwrap()
-    );
-}
-
 pub fn run_proj() {
-    let output = Command::new("cmake")
+    let mut child = Command::new("cmake")
         .arg(".")
         .arg("-B")
         .arg("build")
         .arg("-GNinja")
-        .output()
+        .spawn()
         .unwrap_or_else(|e| {
             eprintln!(
                 "Error when pre building: {e:?}"
@@ -23,12 +16,13 @@ pub fn run_proj() {
 
             exit(0);
         });
-    print_output(output);
 
-    let output = Command::new("cmake")
+    child.wait().unwrap();
+
+    let mut child = Command::new("cmake")
         .arg("--build")
         .arg("build")
-        .output()
+        .spawn()
         .unwrap_or_else(|e| {
             eprintln!(
                 "Error when building: {e:?}"
@@ -36,17 +30,17 @@ pub fn run_proj() {
 
             exit(0);
         });
-    print_output(output);
 
-    let output = Command::new(format!(
+    child.wait().unwrap();
+
+    Command::new(format!(
         "./build/Source/Main/{}",
         get_parent_directory()
     ))
-    .output()
+    .spawn()
     .unwrap_or_else(|e| {
         eprintln!("Error when executing: {e:?}");
 
         exit(0);
     });
-    print_output(output);
 }
